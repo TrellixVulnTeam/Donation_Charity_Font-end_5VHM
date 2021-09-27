eca51fb4eea81d74b2e9e97fb73d6b9c04b0fff6
VuHuynhATM <vuhtse140902@gmail.com>
2021-09-27 11:52:37 +0700

    create Layout

--- Donation-webOr/src/app/app.module.ts
+++ Donation-webOr/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './layout/login/login.component';
+import { HeaderBarComponent } from './layout/header-bar/header-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderBarComponent
   ],
   imports: [
     BrowserModule,
